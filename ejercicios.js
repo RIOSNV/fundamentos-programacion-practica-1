e97ec599081f3dbc2c69764cbc6cc87114bb615c
RIOSNV <rodrigo.rios@tecdesoftware.edu.mx>
2025-11-10 22:59:15 -0600

    ejercicio 5.1

--- ejercicios.js
+++ ejercicios.js
@@ -284,10 +284,17 @@ function invertirArray(arr) {
  * Ejemplo: crearMatriz(2, 3) debe retornar [[0, 0, 0], [0, 0, 0]]
  */
 function crearMatriz(filas, columnas) {
-  // TODO: Crea una matriz de filas x columnas llena de ceros
   const matriz = [];
+  for (let i = 0; i < filas; i++) {
+    const fila = [];
+    for (let j = 0; j < columnas; j++) {
+      fila.push(0);
+    }
+    matriz.push(fila);
+  }
   return matriz;
 }
+
 
 /**
  * Ejercicio 5.2: Suma de elementos de una matriz (6 puntos)
