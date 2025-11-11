5130bd2dfac11de95b359d825ba7cf559423f3ae
RIOSNV <rodrigo.rios@tecdesoftware.edu.mx>
2025-11-10 22:49:24 -0600

    ejercicio 4.3

--- ejercicios.js
+++ ejercicios.js
@@ -233,10 +233,9 @@ function promedioArray(numeros) {
  * @returns {number} - El nÃºmero mÃ¡s grande del array
  */
 function encontrarMaximo(numeros) {
-  // TODO: Encuentra y retorna el nÃºmero mÃ¡s grande
-  // Pista: Puedes usar Math.max(...numeros) o hacerlo con un bucle
-  return 0;
+  return Math.max(...numeros);
 }
+
 
 /**
  * Ejercicio 4.4: Filtrar nÃºmeros mayores a un valor (5 puntos)
